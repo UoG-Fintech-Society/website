8fbcc48940795a5126f57e31ea944a833a5e8865
Nikolas Pitsillos <npitsillos@hotmail.com>
2017-08-14 00:50:26 +0300

    Finished setting up website

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
+app.use(express.static(__dirname + '/template'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+app.get('/',function (req,res){
+    res.sendFile("index.html");
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.post('/subscribe',function(req,res){
+    fs.appendFile('mailinglist.txt',req.body.email+'\n');
+    res.end();
+});
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(1337, function (){
+    console.log("Server running at http://localhost:1337");
+});
---
+++ template/assets/scripts/subscribe.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#email-form").submit(function (e){
+        e.preventDefault();
+        var obj = {};
+        obj['email'] = $("#email").val();
+        console.log(obj)
+        $.ajax({
+            method: 'post',
+            url: '/subscribe',
+            data: obj,
+            success: function(){
+                $("#success").removeClass("hide");
+                $("#form").addClass("hide");
+            }
+        });
+    });
+});
