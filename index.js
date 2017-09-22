470f1512477b68139e6b1f552aa2f0d7a604ea9c
Antreas Pogiatzis <apogiatzis@airpoint.net>
2017-09-22 21:42:53 +0100

    integrated mailchimp subscribers list

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var request = require('superagent')
 
 app.use(express.static(__dirname + '/template'));
 app.use(bodyParser.json());
@@ -14,13 +15,39 @@ app.get('/',function (req,res){
     res.sendFile("index.html");
 });
 
-app.post('/subscribe',function(req,res){
-    fs.appendFile('mailinglist.txt',req.body.email+'\n');
-    res.end();
+// Mailchimp settings 
+var mailchimpInstance   = 'us16',
+    listUniqueId        = '73db9aabe6',
+    mailchimpApiKey     = '5e9927aeec9aa3802e5c21a3b2c6ece7-us16';
+
+app.post('/subscribe', function (req, res) {
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+          'email_address': req.body.email,
+          'status': 'pending'
+        })
+            .end(function(err, response) {
+                if (response.status < 300) {
+                    res.send('SENT_CONFIRMATION');
+                } else if (response.status === 400 && response.body.title === "Member Exists") {
+                    res.send('ALREADY_EXISTS');
+                } else {
+                    res.send('ERROR');
+                }
+          });
 });
+
+// app.post('/subscribe',function(req,res){
+//     fs.appendFile('mailinglist.txt',req.body.email+'\n');
+//     res.end();
+// });
 
 var portNumber =  process.env.port || process.env.PORT || 1337
 
 app.listen(portNumber, function (){
     console.log("Server running at http://localhost:%d",portNumber);
 });
+
--- template/assets/scripts/subscribe.js
+++ template/assets/scripts/subscribe.js
@@ -7,9 +7,20 @@ $("#email-form").submit(function (e){
         method: 'post',
         url: '/subscribe',
         data: obj,
-        success: function(){
-            $("#success").removeClass("hide");
-            $("#form").addClass("hide");
+        success: function(res){
+            if(res == "SENT_CONFIRMATION"){
+                $("#success").removeClass("hide");
+                $("#form").addClass("hide");
+                $("#subscribe-status").html("You will shortly receive a confirmation email. Please use the link to confirm your subscription.")
+            } else if (res == "ALREADY_EXISTS") {
+                $("#success").removeClass("hide");
+                $("#form").addClass("hide");
+                $("#subscribe-status").html("You are already a member of our subscribers list")
+            } else {
+                $("#form").addClass("hide");
+                $("#subscribe-status").html("Something went wrong while subscribing you. Please try again.")
+            }
+            
         }
     });
 });
